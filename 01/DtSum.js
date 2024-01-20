b5a12b2ad830c74583b097eccbf9c69b13c620f6
UditRajputDeveloper <157204494+UditRajputDeveloper@users.noreply.github.com>
2024-01-20 10:10:40 +0000

    Dt

---
+++ 01/DtSum.js
@@ -0,0 +1,9 @@
+// Perimitive Data Type || Call by Value 
+// 7 types : String, Number , boolean, null, undefined , Symbol, BigInt
+
+// Referance type of Data Type || Not Permitive Data Type 
+
+// Array 
+// Objects
+//Functions
+
