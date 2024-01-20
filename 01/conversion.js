06b4ec6c846466d6eafe637504ca2da051fc6aaf
UditRajputDeveloper <157204494+UditRajputDeveloper@users.noreply.github.com>
2024-01-20 09:57:33 +0000

    conversion operation

---
+++ 01/conversion.js
@@ -0,0 +1,35 @@
+// // ************ Operations *********//
+
+// let value = 3
+
+// let negvalue = -value
+
+// // console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 =  'Udit'
+
+// let str2 = 'King'
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(true);
+
+// let num1, num2, num3 
+
+// let gameCounter = 100
+
+// gameCounter++;
+
+// console.log(gameCounter);
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
