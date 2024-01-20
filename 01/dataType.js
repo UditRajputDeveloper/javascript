e3ce0d9aba8d80e72d6fcab6f29e3aba3eb1df19
UditRajputDeveloper <157204494+UditRajputDeveloper@users.noreply.github.com>
2024-01-20 09:21:36 +0000

    code

---
+++ 01/dataType.js
@@ -0,0 +1,16 @@
+"use Strict"; // treat all code as new version
+
+//alert(3 + 3) // we are using node js, not browser
+
+let name = 'Udit'
+let age = 18 
+let isLoggedIn = false
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standaloan value
+// undefined => 
+
+
---
+++ 01/variables.js
@@ -0,0 +1,22 @@
+const accountId = 13950;
+let accountEmail = 'udit@gmail.com';
+var accountPassword = '1234567';
+accountCity = 'Bijnore';
+
+let accountState;
+
+//accountId = 2;
+
+accountEmail = 'abc@gmail.com';
+
+accountPassword = '0000000';
+
+accountCity =  'mohali';
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* 
+ *  Prefer not use to var  
+ *  bcz of issue in block scope  and functional scope
+ *  
+ */
